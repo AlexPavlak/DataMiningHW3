3c8a62743ad21478c243bb88a2554f3308d9c12b
Alex Pavlak <alexander.pavlak@mavs.uta.edu>
2019-04-27 10:55:05 -0500

    Some progress

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,10 +1,28 @@
 #Alexander Pavlak
 #1001572620
 #CSE 4334-001
-#Homework 1: Kernel Density Estimation
+#Homework 3: Logistic Regression
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+def outputFunction(x,w):
+    exponent = w[0] + (w[1]*x[0]) + (w[2]*x[1])
+    result = 1 / (1 + exp(-exponent))
+
+def errorFunction():
+
+def batch(trainingSet,testingSet):
+    weights = np.random.uniform(low=-0.01,high=0.01,3)
+
+    iterations = 0
+    #gradient norm needs to be replaced by the l1 norm of the gradient
+    while(iterations < 100000 or gradientNorm<.001):
+       error = 0
+       for i in trainingSet:
+           output =  outputFunction(i,weights)
+           error += .5 * (i[2] - output)**2
+           gradient = np.array()
 
 #Given parameters for generating 1st set of gausian numbers
 mean1 = np.array([1,0])
